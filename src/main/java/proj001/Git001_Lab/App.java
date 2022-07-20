10e85d168b4150fc4cdda4bba44c1654530e2025
pmjar <pmjar@DESKTOP-ELT6FE7>
2022-07-19 21:29:46 -0500

    First Commit

---
+++ src/main/java/proj001/Git001_Lab/App.java
@@ -0,0 +1,13 @@
+package proj001.Git001_Lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/proj001/Git001_Lab/AppTest.java
@@ -0,0 +1,20 @@
+package proj001.Git001_Lab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
