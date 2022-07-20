d59b033f9bf2686138b822af68d723fc764a923f
pmjar <pmjar@DESKTOP-ELT6FE7>
2022-07-20 09:23:16 -0500

    First Commit

---
+++ src/main/java/proj001/Git001_Lab/Sample.java
@@ -0,0 +1,10 @@
+package proj001.Git001_Lab;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Git and Github...");
+	}
+
+}
